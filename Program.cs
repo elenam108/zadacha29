d299fe57a8bd3529d55d79c593399363b52d98ce
ElenaM <elenam108@yandex.ru>
2022-11-06 21:25:22 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов 
+// с клавиатуры и выводит массив на экран.
+
+Console.Write("Введите колличество элементов массива (x): ");
+int Num = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[Num];
+
+for (int i = 0; i < Num; i++)
+{
+    array[i] = new Random().Next(Num + 1);
+    Console.Write(array[i] + "; ");
+}
+//Console.WriteLine();
+
+
